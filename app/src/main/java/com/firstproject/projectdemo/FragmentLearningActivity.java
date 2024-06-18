82393cba16b37085fff3b415d09950b886364b99
Sujan Rana <khembikram1@gmail.com>
2024-06-18 09:59:53 +0545

    fragment Initialize

--- app/src/main/java/com/firstproject/projectdemo/FragmentLearningActivity.java
+++ app/src/main/java/com/firstproject/projectdemo/FragmentLearningActivity.java
@@ -1,6 +1,8 @@
 package com.firstproject.projectdemo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FragmentLearningActivity extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+    Button btnOne, btnTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,9 @@ public class FragmentLearningActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        frameLayout=findViewById(R.id.framelayout);
+        btnOne = findViewById(R.id.btnFragOne);
+        btnTwo = findViewById(R.id.btnFragTwo);
     }
 }
